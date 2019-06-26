a8141c285b94f37e5f2942d4d9234f08c1310a0d
Gildas Cherruel <gildas.cherruel@genesys.com>
2019-06-26 14:48:14 +0900

    Client Get Server Configuration

---
+++ server_configuration.go
@@ -0,0 +1,25 @@
+package iwt
+
+import (
+	"fmt"
+)
+
+// ServerConfiguration contains information about a PureConnect server
+type ServerConfiguration struct {
+	Version      int                 `json:"cfgVer"`
+	Capabilities map[string][]string `json:"capabilities"`
+}
+
+// GetServerConfiguration fetches the configuration of the PureConnect server
+func (client *Client) GetServerConfiguration() (*ServerConfiguration, error) {
+	results := []struct{Config ServerConfiguration `json:"serverConfiguration"`}{}
+	_, err := client.sendRequest(client.Context, &requestOptions{Path: "/serverConfiguration" }, &results)
+	if err != nil {
+		return nil, err
+	}
+	if len(results) == 0 {
+		return nil, fmt.Errorf("Failed to query")
+	}
+
+	return &results[0].Config, nil
+}
