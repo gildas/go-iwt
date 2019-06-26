59faaeeaf6e7b52ea0c9fbe37c371b8f7cafd42a
Gildas Cherruel <gildas@breizh.org>
2019-06-27 01:14:47 +0900

    added some canned statuses

--- status.go
+++ status.go
@@ -4,16 +4,24 @@ import (
 	"fmt"
 )
 
-
 // Status defines the status of a queue, chat, IWT request
 type Status struct {
 	Type   string `json:"type"`
 	Reason string `json:"reason"`
 }
 
+var (
+	StatusUnknownEntitySession = Status{"failure", "error.websvc.unknownEntity.session"}
+)
+
 // IsOK tells if the status is a success
 func (status Status) IsOK() bool {
 	return status.Type == "success"
+}
+
+// IsA tells if the status is the same as the given status
+func (status Status) IsA(ref Status) bool {
+	return status.Type == ref.Type && status.Reason == ref.Reason
 }
 
 // AsError converts a status to a GO error
